4e76ae0ceae9a8718458fa9e9ce7e9ed14f1350e
Abhishek Verma <69351665+iam-abhi@users.noreply.github.com>
2023-10-20 18:02:00 +0530

    Update .github/workflows/maven-publish.yml

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -4,11 +4,21 @@ public class Pizza {
 
     private int price;
     private Boolean isVeg;
+    private boolean cheeze = true;
+    private  boolean vegtoppings = true;
+    private  boolean nonvegtoppings = true;
+    private boolean take = true;
     private String bill;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
-        // your code goes here
+        if (isVeg){
+            price = 300;
+            bill = "Base Price Of The Pizza: 300"+System.lineSeparator();
+        }else{
+            price =400;
+            bill = "Base Price Of The Pizza: 400"+System.lineSeparator();
+        }
     }
 
     public int getPrice(){
@@ -17,18 +27,48 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if (cheeze) {
+            price = price + 80;
+            cheeze = false;
+        }
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(isVeg){
+            if (vegtoppings){
+            price = price+70;
+            vegtoppings = false;
+            bill += "Extra Toppings Added: 70"+System.lineSeparator();
+            }
+        }else {
+            if (nonvegtoppings){
+            price = price + 120;
+            nonvegtoppings = false;
+            bill += "Extra Toppings Added: 120"+System.lineSeparator();
+            }
+        }
     }
 
     public void addTakeaway(){
-        // your code goes here
+       if (take){
+           price = price+20;
+           take = false;
+       }
+
     }
 
     public String getBill(){
-        // your code goes here
+        if (!cheeze){
+            bill += "Extra Cheese Added: 80"+System.lineSeparator();
+        }if (!vegtoppings){
+            bill += "Extra Toppings Added: 70"+System.lineSeparator();
+        }if (!nonvegtoppings){
+            bill += "Extra Toppings Added: 120"+System.lineSeparator();
+        }if (!take){
+            bill += "Paperbag Added: 20"+System.lineSeparator();
+        }
+        bill += "Total Price: "+getPrice();
         return this.bill;
     }
 }
